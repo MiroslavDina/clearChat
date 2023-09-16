8840b993df88557b7739fa7e46f100bcc939f253
Watermellon86 <91654341+MiroslavDina@users.noreply.github.com>
2023-09-16 23:43:10 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,9 @@
-RegisterCommand("clear", function()
-    TriggerEvent("chat:clear")
-    TriggerEvent("chatMessage", "Chat", {255, 0, 0}, "Chat cleared.")
-end, false)
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Commands.Add('clear', 'Clear Chat', {}, false, function(source, args)
+    TriggerClientEvent('chat:clear', source)
+end, 'user')
+
+QBCore.Commands.Add('clearall', 'Clear All Chat(Admin Only)', {}, false, function(source, args)
+    TriggerClientEvent('chat:clear', -1)
+end, 'admin')
